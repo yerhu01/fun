79fbd7742950e3ff2680fa2d8e9041016b5a55d5
Darian Wang <darianjupiter@gmail.com>
2021-05-10 16:42:10 -0700

    Number of 1 Bits C

---
+++ lc/c/041.c
@@ -0,0 +1,19 @@
+// Drop on bits
+int hammingWeight(uint32_t n) {
+    int ret = 0;
+    while (n) {
+        ret++;
+        n &= n - 1;
+    }
+    return ret;
+}
+
+// Bit shifting
+int hammingWeight(uint32_t n) {
+    int ret = 0;
+    while (n) {
+        ret += n & 1;
+        n >>= 1;
+    }
+    return ret;
+}
